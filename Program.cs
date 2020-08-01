d61785d6ccec4b0a09fe111de0d676377feabaf0
Utharn Buranasaksee <utarn@yahoo.com>
2020-08-01 07:11:05 +0700

    Starter Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pattern_decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = "This is uploading data.";
+        }
+    }
+}
